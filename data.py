9729171517949b12ecd966e5b486158396b23c85
abtion <abtion@outlook.com>
2021-02-04 15:26:50 +0800

    :zap: [update] train.

--- data.py
+++ data.py
@@ -7,15 +7,6 @@ from transformers import BertTokenizer
 tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
 
 
-# 输入的句子结构 两个句子一个标签
-class InputExample(object):
-    def __init__(self, guid, text_a, text_b=None, label=None):
-        self.guid = guid
-        self.text_a = text_a
-        self.text_b = text_b
-        self.label = label
-
-
 #  读取json文件
 def read_json(input_file):
     with open(input_file, 'r', encoding='UTF-8') as f:
@@ -26,53 +17,27 @@ def read_json(input_file):
         return lines
 
 
-def get_examples(lines, set_type):
-    examples = []
-    for (i, line) in enumerate(lines):
-        guid = "%s-%s" % (set_type, i)
-        text_a = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(lines[i]['sentence1']))
-        text_b = None
-        # 有改动
-        label = (line['label']) if set_type != './data/test' else "0"
-        examples.append(InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
-    return examples
-
-
-def get_any_examples(data_dir):
-    return get_examples(read_json(data_dir), os.path.splitext(data_dir)[0])
-
-
-# 获取标签
-def get_labels():
-    labels = []
-    for i in range(2):
-        # if i == 5 or i == 11:
-        #     continue
-        labels.append(str(i))
-    return labels
-
-
 class BertClfDataset(Dataset):
     """
     用于BERT文本分类的数据集，该处定义其数据结构
     """
 
-    def __init__(self, data):
+    def __init__(self, fp):
         """
         bert的输入需要以下三种数据，labels为文本类别用于训练及评测。
         :param data:
         """
-        self.input_ids = data['input_ids']
-        self.attention_masks = data['attention_masks']
-        self.token_type_ids = data['token_type_ids']
-        self.input_labels = data['input_labels']
+        data = read_json(fp)
+        self.text_a = [line['sentence1'] for line in data]
+        self.text_b = [line['sentence2'] for line in data]
+        self.labels = [int(line['label']) for line in data]
 
     def __len__(self):
         """
         数据集总大小
         :return:
         """
-        return len(self.input_labels)
+        return len(self.labels)
 
     def __getitem__(self, index):
         """
@@ -80,8 +45,7 @@ class BertClfDataset(Dataset):
         :param index: 索引
         :return:
         """
-        # print(self.input_ids[idex])
-        return self.input_ids[index], self.attention_masks[index], self.token_type_ids[index], self.input_labels[index]
+        return self.text_a[index], self.text_b[index], self.labels[index]
 
 
 def get_loader(data, batch_size=4, shuffle=False):
@@ -97,7 +61,7 @@ def get_loader(data, batch_size=4, shuffle=False):
     # print(dataset.__len__())
     # for i in range(dataset.__len__()):
     #     print(dataset.__getitem__(i))
-    loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=_collate_fn)
+    loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=_collate_fn,num_workers=4)
     return loader
 
 
@@ -107,54 +71,8 @@ def _collate_fn(data):
     :param data:
     :return:
     """
-    input_ids, attention_masks, token_type_ids, labels = zip(*data)
-    input_ids = torch.tensor(input_ids).long()
-    attention_masks = torch.tensor(attention_masks).long()
-    token_type_ids = torch.tensor(token_type_ids).long()
-    labels = torch.tensor(labels).long()
-    return input_ids, attention_masks, token_type_ids, labels
+    text_a, text_b, labels = zip(*data)
+    inputs = tokenizer(text_a, text_b, padding=True, return_tensors='pt')
+    labels = torch.tensor(labels, dtype=torch.long)
 
-
-def preprocess(data_dir,max_length=512):
-    """
-    处理数据
-    :param raw_data_fn: 原始数据文件名
-    :return:
-    """
-    examples = get_any_examples(data_dir)
-
-    id_list = []
-    attention_masks_list = []
-    token_type_ids_list = []
-    labels_list = []
-
-    for example in examples:
-
-        input_ids = example.text_a
-
-        label_list = get_labels()
-        ################# 有更改
-        label_map = {label: i for i, label in enumerate(label_list)}
-        label = label_map[example.label] ########有改动 将str转化为int索引
-
-        if len(input_ids) > max_length:
-            token = tokenizer.tokenize(input_ids)
-            input_ids = token[:max_length - 2]
-
-        input_len = len(input_ids)
-        padding_length = max_length - input_len
-
-        attention_masks = [1] * input_len + [0] * padding_length
-        input_ids = input_ids + ([0] * padding_length)
-        token_type_ids = [0] * max_length
-
-        id_list.append(input_ids)
-        attention_masks_list.append(attention_masks)
-        token_type_ids_list.append(token_type_ids)
-        labels_list.append(label)
-
-    data = {'input_ids': id_list,
-            'attention_masks': attention_masks_list,
-            'token_type_ids': token_type_ids_list,
-            'input_labels': labels_list}
-    return data
+    return inputs, labels
--- main.py
+++ main.py
@@ -1,51 +1,48 @@
 import torch
 import os
-from data import get_loader, preprocess, get_any_examples
-from transformers import BertForSequenceClassification, BertConfig
 import numpy as np
+from data import get_loader
+from transformers import BertForSequenceClassification, BertConfig
 
-
-#os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "3"
 # 定义配置项
 cache_dir = './data'
-path = './data/bert.pt'
+ckpt_path = './data/bert.pt'
 model_name = 'bert-base-chinese'
 train_dir = "./data/train.json"
 test_dir = "./data/test.json"
 dev_dir = "./data/dev.json"
 valid_dir = "./data/dev.json"
-batch_size = 8
-accumulation_steps = 8
-lr = 2e-3
+batch_size = 32
+log_steps = 50
+lr = 1e-5
 
-epochs = 3  # 10
+epochs = 10  # 10
 # lr = 2e-3 batchsize = 8 accuracy = 0.68999 epoch = 2
-device = torch.device(f'cuda:{2}' if torch.cuda.is_available() else 'cpu')
+device = torch.device(f'cuda:{1}' if torch.cuda.is_available() else 'cpu')
 print("device:", device)
+
+
 def train_entry():
     # 加载数据
-    train_data = preprocess(train_dir)
-    #valid_data = preprocess(valid_dir)
-    test_data = preprocess(valid_dir)
-
-    train_loader = get_loader(train_data, batch_size=batch_size, shuffle=True)
-    #dev_loader = get_loader(valid_data,batch_size=batch_size, shuffle=False)
+    train_loader = get_loader(train_dir, batch_size=batch_size, shuffle=True)
+    # dev_loader = get_loader(valid_data,batch_size=batch_size, shuffle=False)
     # 测试集无需打乱顺序
-    test_loader = get_loader(test_data, batch_size=8, shuffle=False)
+    test_loader = get_loader(dev_dir, batch_size=8, shuffle=False)
 
     # 加载模型及配置方法                                              15
     bert_config = BertConfig.from_pretrained(model_name, num_labels=2)  # 头条文本分类数据集为15类
-    model = BertForSequenceClassification.from_pretrained(pretrained_model_name_or_path=model_name, config=bert_config, cache_dir=cache_dir)
+    model = BertForSequenceClassification.from_pretrained(pretrained_model_name_or_path=model_name,
+                                                          config=bert_config)
     model.to(device)
     print("model device:", model.device)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    train(model, optimizer, train_loader)
-    # torch.load('/data/fangmiaoTEST/zaz/bert/bert.pt')
+    train(model, optimizer, train_loader, test_loader)
+    torch.load(ckpt_path)
     test(model, test_loader)
-    torch.save(model.state_dict(), '/data/fangmiaoTEST/zaz/bert/bert.pt')
 
 
-def train(model, optimizer, data_loader):
+def train(model, optimizer, data_loader, test_loader):
     """
     训练，请自行实现
     :param model:
@@ -53,36 +50,37 @@ def train(model, optimizer, data_loader):
     :param data_loader:
     :return:
     """
-    min_loss = float('inf')
-    model.train()
+    best_acc = 0.0
     for e in range(epochs):
-        model.zero_grad()
+        train_acc = [0.0]
+        train_loss = [0.0]
+        model.train()
         for step, batch in enumerate(data_loader):
-            inputs = {'input_ids':batch[0],
-                      'attention_mask':batch[1],
-                      'token_type_ids':batch[2],
-                      'labels':batch[3]}
-            inputs['input_ids'] = inputs['input_ids'].to(device)
-            inputs['labels'] = inputs['labels'].to(device)
-            inputs['attention_mask'] = inputs['attention_mask'].to(device)
-            inputs['token_type_ids'] = inputs['token_type_ids'].to(device)
-            output = model(input_ids = inputs['input_ids'], labels = inputs['labels'])
-            train_loss = output[0]
-            # train_loss.backward(retain_graph=True)
-            train_loss.backward()
-            optimizer.step()
             optimizer.zero_grad()
+            x, y = batch
+            x.to(device)
+            y = y.to(device)
+            outputs = model(**x, labels=y, return_dict=True)
+            outputs.loss.backward()
+            optimizer.step()
 
+            train_acc.append(torch.mean((y == torch.argmax(outputs.logits, dim=-1)).float()).cpu().item())
+            train_loss.append(outputs.loss.cpu().item())
 
-            if (step+1) % accumulation_steps == 1:
-                print('Train Epoch: {} [{}/{}]  ||  train_loss: {:.6f}'.format(
-                    e+1, step * batch_size, len(data_loader.dataset), train_loss.item()
+            if (step + 1) % log_steps == 1:
+                print('Train Epoch: {} [{}/{}]  ||  train_loss: {:.6f} || train_acc: {:.6f}'.format(
+                    e + 1, step * batch_size, len(data_loader.dataset), np.mean(train_loss[-log_steps:]),
+                    np.mean(train_acc[-log_steps:])
                 ))
 
-        print('Train Epoch: {} || train_loss:{:.6f}.'.format(e+1, train_loss.item()))
-        # if train_loss < min_loss:
-        #     min_loss = train_loss
-        #     torch.save(model.state_dict(), '/data/fangmiaoTEST/zaz/bert/bert.pt')
+        print('Train Epoch: {} || train_loss: {:.6f} || train_acc: {:.6f}'.format(e + 1, np.mean(train_loss),
+                                                                                  np.mean(train_acc)))
+
+        test_acc = test(model, test_loader)
+        if test_acc > best_acc:
+            best_acc = test_acc
+            torch.save(model.state_dict(), ckpt_path)
+            print('model saved.')
 
 
 def test(model, test_loader):
@@ -93,44 +91,19 @@ def test(model, test_loader):
     :return:
     """
     accuracy = 0.0
-    sums = 0.0 # 所有计算精度的总和
-    nums = 0
-    for step, batch in enumerate(test_loader):
-        model.eval()
-        with torch.no_grad():
-            inputs = {'input_ids': batch[0],
-                      'attention_mask': batch[1],
-                      'token_type_ids': batch[2],
-                      'labels': batch[3]
-                      }
-            inputs['input_ids'] = inputs['input_ids'].to(device)
-            inputs['labels'] = inputs['labels'].to(device)
-            inputs['attention_mask'] = inputs['attention_mask'].to(device)
-            inputs['token_type_ids'] = inputs['token_type_ids'].to(device)
-            output = model(input_ids=inputs['input_ids'], labels=inputs['labels'])
-            test_loss, logits = output[:2]
-            preds = logits.detach().cpu().numpy()
-            predict_label = np.argmax(preds, axis=1)
-            predict_label = torch.tensor(predict_label)
-            predict_label = predict_label.to(device)
-            #计算精度
-            sum = 0.0  # 每一次的和
-            nums = nums + predict_label.numel()  # 计算预测的次数
-            sum = predict_label.eq(inputs['labels']).float().sum()
-            sum = sum.item()
-            sums = sums + sum
-            accuracy = sums/nums
-            # print("the number of true result of this epoch is(sum) :", sum)
-            # print("all the true result is(sums) :", sums)
-            # print("inputs['labels']:", inputs['labels'].shape)
-            # print("number of inputs is:", nums)
-            # print("test_loss:", test_loss)
-            print("accuracy of test is-->sum/nums :", accuracy)
-
-    # for *x, y in test_loader:
-    #     outputs = model(*x, labels=y)
-    #     print(outputs[0])
-
+    loss = 0.0
+    model.eval()
+    with torch.no_grad():
+        for step, batch in enumerate(test_loader):
+            x, y = batch
+            x.to(device)
+            y = y.to(device)
+            outputs = model(**x, labels=y, return_dict=True)
+            accuracy += torch.sum(y == torch.argmax(outputs.logits, dim=-1)).cpu().item()
+            loss += outputs.loss.cpu().item()
+        print(f'valid loss: {loss / len(test_loader)}')
+        print(f'valid acc: {accuracy / test_loader.dataset.__len__()}')
+    return accuracy
 
 
 if __name__ == '__main__':
